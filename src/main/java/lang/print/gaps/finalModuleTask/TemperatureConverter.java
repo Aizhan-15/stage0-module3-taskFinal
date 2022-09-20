4213605b305568a9ec26a120b4fef4359ac0035f
Aizhan-15 <aizhanurman@gmail.com>
2022-09-20 16:26:33 +0600

    completed all the tasks

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -3,10 +3,13 @@ package lang.print.gaps.finalModuleTask;
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius) {
         {
-            float Fahrenheit, Celsius;
+            float Fahrenheit, Celsius, Celsius2, Fahrenheit2;
             Celsius = 5;
+            Celsius2 = 1;
             Fahrenheit = ((Celsius * 9) / 5) + 32;
+            Fahrenheit2 = ((Celsius2 * 9) / 5) + 32;
             System.out.println(Fahrenheit);
+            System.out.println(Fahrenheit2);
         }
     }
 }
